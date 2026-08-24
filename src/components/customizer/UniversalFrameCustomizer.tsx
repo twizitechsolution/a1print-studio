8433f8baee5ba5b33555f4bcf3a6616cc4de70b5
twizitechsolution <twizitechsolution@gmail.com>
2026-08-25 01:18:56 +0530

    Fix missing useEffect import in UniversalFrameCustomizer.tsx to resolve useEffect is not defined runtime crash

--- src/components/customizer/UniversalFrameCustomizer.tsx
+++ src/components/customizer/UniversalFrameCustomizer.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { UniversalFrameTemplate } from '../../types/template';
 import { PhotoCropModal } from './PhotoCropModal';
 import { generateHighResPrintFile } from '../../utils/printExporter';
